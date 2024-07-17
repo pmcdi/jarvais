c43988146b24782bfdd12f35cb63d4fa344f9c01
JoshuaSiraj <joshua.siraj@ryerson.ca>
2024-07-17 16:09:40 -0400

    update to analyzer and trainer

--- AutoML/analyzer.py
+++ AutoML/analyzer.py
@@ -1,4 +1,4 @@
-import os
+import os, warnings
 
 import pandas as pd
 import seaborn as sns
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from tableone import TableOne
 import numpy as np
 
-from pandas.api.types import is_numeric_dtype, is_string_dtype
+from pandas.api.types import is_numeric_dtype
 
 from typing import Union
 
@@ -18,6 +18,7 @@ class AutoMLAnalyzer():
     def __init__(self,
                  data: pd.DataFrame, 
                  target_variable: Union[str, None] = None,
+                 categorical: Union[list, None] = None,
                  mapping_file: Union[str, os.PathLike, None] = None,
                  output_dir: Union[str, os.PathLike] = '.'):
         """
@@ -29,10 +30,15 @@ class AutoMLAnalyzer():
         -----------
         data : pd.DataFrame
             The input data to analyze.
-        target_variable : str
-            The target variable in the dataset.
+        target_variable : str, optional
+            The target variable in the dataset. Default is None.
+        categorical: Union[list, None], optional 
+            Categorical columns in data. Default is None, and categorical and numerical columns will be infered. 
+            If provided remaining columns are infered as numerical.
         mapping_file : Union[str, os.PathLike, None], optional
             Path to a CSV file that contains mappings for data cleaning. Default is None.
+        output_dir: Union[str, os.PathLike], optional
+            Output directory for analysis. Default is current directory.
 
         Raises:
         -------
@@ -59,14 +65,33 @@ class AutoMLAnalyzer():
         else:
             self.mapping_file = mapping_file
 
-        self.numeric_columns = [col for col in self.data.columns if is_numeric_dtype(self.data[col])]
-        self.categorical_columns = [col for col in self.data.columns if is_string_dtype(self.data[col])]
-
-        if self.target_variable is not None and 1.*self.data[self.target_variable].nunique()/self.data[self.target_variable].count() < 0.04:
-            # Heuristic to check if target variable is categorical, https://stackoverflow.com/questions/35826912/what-is-a-good-heuristic-to-detect-if-a-column-in-a-pandas-dataframe-is-categori
-            self.categorical_columns.append(self.target_variable)
+        if categorical is None:
+            self._infer_types()
+            warnings.warn(f'Categorical columns not defined, used a heuristic to define categorical and continuous columns. Please review!\nCategorical: {self.categorical_columns}\nContinuous: {self.continuous_columns}')
+        else:
+            self.categorical_columns = categorical
+            self.continuous_columns = list(set(self.data.columns) - set(categorical))
+            self.data[self.continuous_columns] = self.data[self.continuous_columns].applymap(lambda x: pd.to_numeric(x, errors='coerce')) # Replace all non numerical values with NaN
 
         self.outlier_analysis = '' # Used later to write to PDF
+
+    def _infer_types(self):
+        # adapted from https://github.com/tompollard/tableone/blob/main/tableone/preprocessors.py
+
+        # assume all non-numerical and date columns are categorical
+        numeric_cols = set([col for col in self.data.columns if is_numeric_dtype(self.data[col])])
+        date_cols = set(self.data.select_dtypes(include=[np.datetime64]).columns)
+        likely_cat = set(self.data.columns) - numeric_cols
+        likely_cat = list(likely_cat - date_cols)
+
+        # check proportion of unique values if numerical
+        for var in self.data._get_numeric_data().columns:
+            likely_flag = 1.0 * self.data[var].nunique()/self.data[var].count() < 0.01
+            if likely_flag:
+                likely_cat.append(var)
+
+        self.continuous_columns = list(set(self.data.columns) - set(likely_cat))
+        self.categorical_columns = likely_cat
 
     def _run_janitor(self):
         
@@ -81,6 +106,7 @@ class AutoMLAnalyzer():
                 outliers = category_counts[category_counts < threshold].index.tolist()
 
                 if len(outliers) > 0:
+                    outliers = [f'{o}: {category_counts[o]} out of {self.data[cat].count()}' for o in outliers]
                     print(f'  - Outliers found in {cat}: {outliers}')
                     self.outlier_analysis += f'  - Outliers found in {cat}: {outliers}\n'
                 else:
@@ -100,6 +126,9 @@ class AutoMLAnalyzer():
     def _create_multiplots(self):
 
         self.multiplots = [] # Used to save in PDF later
+
+        if not os.path.exists(os.path.join(self.output_dir, 'multiplots')): # To save multiplots
+            os.mkdir(os.path.join(self.output_dir, 'multiplots'))
 
         def prep_for_pie(df, label):
 
@@ -137,9 +166,15 @@ class AutoMLAnalyzer():
             
             fontsize = calculate_fontsize(num_categories)
 
-            fig, ax = plt.subplots()
+            cont_columns = [cont for cont in self.data.columns if cont not in self.categorical_columns]
+            n = len(cont_columns)
 
-            ax.pie(values, 
+            rows = int(np.ceil(np.sqrt(n)))
+            cols = int(np.ceil((n) / rows))
+            fig, ax = plt.subplots(rows, cols, figsize=(24, 18)) 
+            ax = ax.flatten() 
+
+            ax[0].pie(values, 
                         labels=labels, 
                         autopct=autopct, 
                         startangle=90,
@@ -147,38 +182,61 @@ class AutoMLAnalyzer():
                         textprops={'fontsize': fontsize},
                         colors=plt.cm.Set2.colors) # 90 = 12 o'clock, 0 = 3 o'clock, 180 = 9 o'clock
 
-            ax.set_title(f"{var} Distribution")
+            ax[0].set_title(f"{var} Distribution")
+            
+            for i in range(1, n):
+
+                # # calculate medians and number of observations
+                # medians = self.data.groupby(var)[cont_columns[i]].mean().values
+                # nobs = self.data[var].value_counts().values
+                # nobs = [str(x) for x in nobs.tolist()]
+                # nobs = ["n: " + i for i in nobs]
+            
+                sns.violinplot(x=var, y=cont_columns[i], data=self.data, ax=ax[i], inner="point")
+                ax[i].tick_params(axis='x', labelrotation=67.5)
+                ax[i].set_title(f"{var} vs {cont_columns[i]}")
+
+                # # Add text to the figure
+                # pos = range(len(nobs))
+                # for tick in pos:
+                #     ax[i].text(pos[tick], medians[tick], nobs[tick],
+                #                 horizontalalignment='center',
+                #                 size='small',
+                #                 color='black')
+
+            
+            # Hide any empty subplots
+            for j in range(n, len(ax)):
+                ax[j].axis('off')
+
             plt.tight_layout()
             
-            fig.savefig(os.path.join(self.output_dir, f'{var}_piechart.png'))
-            plt.close(fig)
-            self.multiplots.append(os.path.join(self.output_dir, f'{var}_piechart.png'))
-            
-            for cont in [cont for cont in self.data.columns if cont not in self.categorical_columns]:
-
-                sns.violinplot(x=var, y=cont, data=self.data)
-                plt.xticks(rotation=67.5)
-                plt.title(f"{var} vs {cont}")
-                plt.tight_layout()
-
-                # plt.show()
-                plt.savefig(os.path.join(self.output_dir, f'{var}_{cont}_multiplots.png'))
-                self.multiplots.append(os.path.join(self.output_dir, f'{var}_{cont}_multiplots.png'))
-                plt.close()
+            # plt.show()
+            plt.savefig(os.path.join(self.output_dir, 'multiplots', f'{var}_multiplots.png'))
+            self.multiplots.append(os.path.join(self.output_dir, 'multiplots', f'{var}_multiplots.png'))
+            plt.close()
 
     def _create_output_pdf(self):
 
         pdf = FPDF()
 
         pdf.add_page()
+        
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        
+        font_path = os.path.join(script_dir, 'fonts/DejaVuSans.ttf')
+        pdf.add_font("dejavu-sans", style="", fname=font_path)
+        
+        font_path = os.path.join(script_dir, 'fonts/DejaVuSans-Bold.ttf')
+        pdf.add_font("dejavu-sans", style="b", fname=font_path)
 
-        pdf.set_font('Times', '', 24)  
+        pdf.set_font('dejavu-sans', '', 24)  
         pdf.write(5, "Analysis Report\n\n")
 
         if self.mapping_file is None:
-            pdf.set_font('Times', '', 12)  
+            pdf.set_font('dejavu-sans', '', 12)  
             pdf.write(5, f"Outlier Analysis:\n")
-            pdf.set_font('Times', '', 10) 
+            pdf.set_font('dejavu-sans', '', 10) 
             pdf.write(5, self.outlier_analysis)
         
         pdf.image(os.path.join(self.output_dir, 'pairplot.png'), Align.C, w=pdf.epw-20)
@@ -187,13 +245,12 @@ class AutoMLAnalyzer():
         pdf.image(os.path.join(self.output_dir, 'pearson_correlation.png'), Align.C, h=pdf.eph/2)
         pdf.image(os.path.join(self.output_dir, 'spearman_correlation.png'), Align.C, h=pdf.eph/2)
 
-        if len(self.multiplots) > 0: # Don't want to add a page if not needed
+        for plot in self.multiplots:
             pdf.add_page()
-            for plot in self.multiplots:
-                pdf.image(plot, Align.C, w=pdf.epw-20, h=pdf.eph/2)
+            pdf.image(plot, Align.C, w=pdf.epw-20, h=pdf.eph)
 
         pdf.add_page()
-        pdf.set_font('Times', '', 14)  
+        pdf.set_font('dejavu-sans', 'b', 14)  
         pdf.write_html(self.mytable.tabulate(tablefmt = "html"), table_line_separators=True)
 
         pdf.output(os.path.join(self.output_dir, 'analysis_report.pdf'))
@@ -211,7 +268,7 @@ class AutoMLAnalyzer():
         self.mytable = TableOne(self.data, categorical=self.categorical_columns, pval=False)
         print(self.mytable.tabulate(tablefmt = "fancy_grid"))
         self.mytable.to_csv(os.path.join(self.output_dir, 'tableone.csv'))
-            
+        
         if self.target_variable in self.categorical_columns: # No point in using target as hue if its not a categorical variable
             g = sns.pairplot(self.data, hue=self.target_variable)
         else:
@@ -222,12 +279,12 @@ class AutoMLAnalyzer():
         plt.savefig(figure_path)
         plt.close()
 
-        pearson_correlation = self.data[self.numeric_columns].corr(method='pearson')
+        pearson_correlation = self.data[self.continuous_columns].corr(method='pearson')
 
-        plt.figure(figsize=(8, 6))
+        plt.figure(figsize=(16,12))
         mask = np.triu(np.ones_like(pearson_correlation, dtype=bool)) # Keep only lower triangle
         np.fill_diagonal(mask, False)
-        sns.heatmap(pearson_correlation, mask=mask, annot=True, cmap='coolwarm', vmin=-1, vmax=1, linewidth=.5, square=True)
+        g = sns.heatmap(pearson_correlation, mask=mask, annot=True, cmap='coolwarm', vmin=-1, vmax=1, linewidth=.5, fmt="1.2f")
         plt.title(f'Pearson Correlation Matrix')
         plt.tight_layout()
 
@@ -235,12 +292,12 @@ class AutoMLAnalyzer():
         plt.savefig(figure_path)
         plt.close()
 
-        spearman_correlation = self.data[self.numeric_columns].corr(method='spearman')
+        spearman_correlation = self.data[self.continuous_columns].corr(method='spearman')
 
-        plt.figure(figsize=(8, 6))
+        plt.figure(figsize=(16,12))
         mask = np.triu(np.ones_like(spearman_correlation, dtype=bool)) # Keep only lower triangle
         np.fill_diagonal(mask, False)
-        sns.heatmap(spearman_correlation, mask=mask, annot=True, cmap='coolwarm', vmin=-1, vmax=1, linewidth=.5, square=True)
+        g = sns.heatmap(spearman_correlation, mask=mask, annot=True, cmap='coolwarm', vmin=-1, vmax=1, linewidth=.5, fmt="1.2f")
         plt.title(f'Spearman Correlation Matrix')
         plt.tight_layout()
 
--- AutoML/trainer.py
+++ AutoML/trainer.py
@@ -1,296 +1,113 @@
-import yaml
-import numpy as np
+import os
+
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
-from sklearn.experimental import enable_halving_search_cv 
-from sklearn.model_selection import train_test_split, HalvingGridSearchCV, RandomizedSearchCV, GridSearchCV
-from sklearn.linear_model import LogisticRegression, LinearRegression
-from sklearn.ensemble import GradientBoostingClassifier, GradientBoostingRegressor, RandomForestClassifier, RandomForestRegressor
-from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
-from sklearn.svm import SVC, SVR
-from sklearn.neural_network import MLPClassifier
-from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
-from sklearn.cluster import KMeans, AffinityPropagation, SpectralClustering
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from typing import Union
 
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from sklearn.base import clone
+from sklearn.model_selection import train_test_split
 
-from tabulate import tabulate
+from autogluon.tabular import TabularPredictor
 
-from .hparams import get_hyperparameters
-from .eval import plot_classification_diagnostics, plot_clustering_diagnostics, plot_regression_diagnostics
+from .eval import plot_classification_diagnostics, plot_regression_diagnostics
 
 class AutoMLSupervised():
     def __init__(self, 
-                 task: str = "classifier",
-                 cv: int = 5,
-                 scaling_method: str='standard',
-                 search: str='gridsearch',
-                 config_path: str='configs/config.yaml'):
+                 task: Union[str, None] = None,
+                 output_dir: Union[str, os.PathLike] = '.'
+                ):
         """
         Initialize the AutoMLTrainer class with specified configurations.
 
         Parameters
         ----------
-        task : str, default='classifier'
-            The type of task to handle. Options are: 'classifier', 'regression'.
-        cv : int, default=5
-            Number of cross-validation folds.
-        scaling_method : str, default='standard'
-            Normalization method. Options are: 'standard', 'minmax', 'none'.
-        search : str, default='gridsearch'
-            Hyperparameter search method. Options are: 'halvinggrid', 'grid', 'random'.
-        config_path : str, default='config.yaml'
-            Path to the YAML configuration file.
+        task : str, default=None
+            The type of task to handle. Options are 'binary', 'multiclass', 'regression', 'quantile'. By default infered by Autogluon
 
         Raises
         ------
         ValueError
             If the task parameter is not one of the specified options.
         """
-        self.cv = cv
-        self.task = task.lower()
-        self.search = search.lower()
-        self.scaling_method = scaling_method.lower()
+        self.output_dir = output_dir
+        self.task = task
 
-        with open(config_path, 'r') as file:
-            config = yaml.safe_load(file)
-
-        if task not in config:
-            raise ValueError("Invalid task parameter. Choose one of: 'classifier', 'regression'")
-
-        task_config = config[task]
-        self.models = {name: eval(model)() for name, model in task_config['models'].items()}
-        self.metrics = task_config['metrics']
-
-        if not self.models or not self.metrics:
-            raise ValueError("Models, metrics, or hparams configuration is missing or incorrect.")
+        if task not in ['binary', 'multiclass', 'regression', 'quantile', None]:
+            raise ValueError("Invalid task parameter. Choose one of: 'binary', 'multiclass', 'regression', 'quantile'. Or provide nothing and let Autogluon infer the task.")
         
-        self.best_model = None # Definition for plotting check later
-
-    def _get_hparams(self, model: str):
-        """
-        Returns hyperparameters for finetuning.
-        """
-        if model in self.hparams:
-            return self.hparams[model]
-        else:
-            raise NotImplementedError(f"No hyperparameters defined for model: {model}")
-
-    def _get_nparams(self, hparams):
-        """
-        Calculates the total number of hyperparameter combinations.
-        """
-        if isinstance(hparams, dict):
-            n = 1
-            for h in hparams:
-                n *= len(hparams[h])
-        elif isinstance(hparams, list):
-            n = [1. for i in range(len(hparams))]
-            for i, hparam in enumerate(hparams):
-                for h in hparam:
-                    n[i] *= len(hparam[h])
-            n = sum(n)
-        return n
-
-    def _get_model(self, model, hparams, kwargs):
-        """
-        Returns the appropriate search model.
-        """
-        if 'halvinggrid' in self.search:
-            return HalvingGridSearchCV(model, hparams, factor=2, scoring=self.metrics[-1], cv=self.cv, error_score=0.)
-        elif 'grid' in self.search:
-            return GridSearchCV(model, hparams, **kwargs)
-        elif 'random' in self.search:
-            return RandomizedSearchCV(model, hparams, **kwargs)
-
-    def _normalize(self, X, y, 
-                  method: str = 'standard'):
-        """
-        params
-        ------
-        method
-            Which normalization method to use? One of ['standard', 'minmax']
-
-        returns
-        -------
-        X
-        """
-        # initialize self.profile containing mean and variance for each feature
-        self.profile = {} 
-
-        # set method
-        if method == 'standard':
-            scaler = StandardScaler()
-        elif method == 'minmax':
-            scaler = MinMaxScaler()
-        else:
-            return X, y
-
-        # convert to numpy array
-        if isinstance (X, (pd.DataFrame, pd.Series)):
-            X_arr = X.to_numpy()
-        else:
-            X_arr = X        
-
-        # transform
-        X = scaler.fit_transform(X_arr)
-
-        if isinstance(y, (pd.DataFrame, pd.Series)):
-            y = y.to_numpy(dtype='float64')
-
-        return X, y
-
-    def _evaluate_model(self, k, kwargs):
-        model = self.models[k]
-
-        # get+count hparams
-        hparams = self._get_hparams(k)
-        n_params = self._get_nparams(hparams)
-
-        # perform hparam search
-        print(f"\nIterating search through {n_params} hyperparameters for {k}.")
-
-        model = self._get_model(model, hparams, kwargs)
-        clf = model.fit(self.X_train, self.y_train)
-
-        result = {
-            'model_key': k,
-            'cv_results': clf.cv_results_,
-            'best_estimator': clone(clf.best_estimator_),
-            'scores': {},
-            'refit_time': clf.refit_time_,
-            'scorer': clf.scorer_,
-            'train_score': {},
-            'test_score': {}
-        }
-
-        # Save metrics
-        for metric in clf.scorer_:
-            train_score = clf.scorer_[metric](clf, self.X_train, self.y_train)
-            result['train_score'][metric] = train_score
-            result['scores'][metric] = clf.scorer_[metric](clf, self.X_test, self.y_test)
-
-        return result
-
-    def _display_results(self, result):
-        headers = ["Metric", "Train Score", "Test Score"]
-        table = []
-
-        for idx, metric in enumerate(result['scores']):
-            train_score = result['train_score'][metric]
-            test_score = result['scores'][metric]
-            if idx == len(result['scores'])-1: # USING LAST METRIC FOR PERFORMANCE, NEED TO CHANGE
-                self.best_metric[result['model_key']] = test_score
-            table.append([metric, f"{train_score:.4f}", f"{test_score:.4f}"])
-
-        print(f"<< {result['model_key']} >> -- took {result['refit_time']:.5f}s to refit.")
-        print(tabulate(table, headers, tablefmt="pretty"))
-    
-    def plot_results(self, model: str = None):
-
-        if self.best_model is None:
-            raise Exception('Best model not defined, run fit first')
+    def _plot_feature_importance(self):
         
-        if model is None:
-            model_to_plot = self.best_model
-        else:
-            model_to_plot = self.best_models[model]
+        df = self.predictor.feature_importance(pd.concat([self.X_test, self.y_test], axis=1))
 
-        model_to_plot.fit(self.X_train, self.y_train)
+        
+        # Plotting
+        fig, ax = plt.subplots(figsize=(10, 6))
 
-        if self.task == 'classifier':
-            plot_classification_diagnostics(model_to_plot, self.X_test, self.y_test, self.data_columns)
-        elif self.task == 'regression':
-            plot_regression_diagnostics(model_to_plot, self.X_test, self.y_test, self.data_columns)
-   
-    def fit(self, data, target_variable=None, 
+        # Adding bar plot with error bars
+        bars = ax.bar(df.index, df['importance'], yerr=df['stddev'], capsize=5, color='skyblue', edgecolor='black')
+
+        # Adding p_value significance indication
+        for i, (bar, p_value) in enumerate(zip(bars, df['p_value'])):
+            height = bar.get_height()
+            significance = '*' if p_value < 0.05 else ''
+            ax.text(bar.get_x() + bar.get_width() / 2.0, height, significance, ha='center', va='bottom', fontsize=12, color='red')
+
+        # Labels and title
+        ax.set_xlabel('Feature')
+        ax.set_ylabel('Importance')
+        ax.set_title('Feature Importance with Standard Deviation and p-value Significance')
+        ax.axhline(0, color='grey', linewidth=0.8)
+        ax.set_xticks(np.arange(len(df.index.values)))
+        ax.set_xticklabels(df.index.values, rotation=45)
+
+        # Show plot
+        plt.tight_layout()
+        plt.show()
+
+    def run(self, data, target_variable,
             test_size: float = 0.2,
-            n_iter: int = 25,
             exclude: list = [], stratify_on=''):
-        """
-        params
-        ------
-        test_size
-            float representing fraction of dataset to hold-out as the test set
-        """
-
-        exclude.append(target_variable)
-        X = data.drop(columns=exclude) # exclude others marked and target
         
-        self.hparams = get_hyperparameters(data, self.task)
+            exclude.append(target_variable)
+            X = data.drop(columns=exclude) 
+            y = data[target_variable]
 
-        self.data_columns = X.columns
+            self.data_columns = X.columns
 
-        y = data[target_variable]
-
-        if y.value_counts().min() > 1: # Meaning it can be used to stratify, if this condition is not met train_test_split produces - ValueError: The least populated class in y has only 1 member, which is too few. The minimum number of groups for any class cannot be less than 2.
-            if stratify_on is not None:
-                stratify_col = y.astype(str) + '_' + data[stratify_on].astype(str) 
+            if y.value_counts().min() > 1: # Meaning it can be used to stratify, if this condition is not met train_test_split produces - ValueError: The least populated class in y has only 1 member, which is too few. The minimum number of groups for any class cannot be less than 2.
+                if stratify_on is not None:
+                    stratify_col = y.astype(str) + '_' + data[stratify_on].astype(str) 
+                else:
+                    stratify_col = y                
             else:
-                stratify_col = y                
-        else:
-            if stratify_on is not None:
-                stratify_col = data[stratify_on]
+                    if stratify_on is not None:
+                        stratify_col = data[stratify_on]
+                    else:
+                        stratify_col = None
+
+            self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=test_size, stratify=stratify_col, random_state=42)
+
+            if self.task == 'binary':
+                eval_metric = 'roc_auc'
             else:
-                stratify_col = None
+                eval_metric = None # Let it infer
 
-        assert X.ndim == 2
 
-        X, y = self._normalize(X, y, method=self.scaling_method) 
+            self.predictor = TabularPredictor(label=target_variable,
+                                         problem_type=self.task,
+                                         eval_metric=eval_metric,
+                                         ).fit(pd.concat([self.X_train, self.y_train], axis=1))
 
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=test_size, stratify=stratify_col, random_state=42)
+            print('\nModel Leaderbord\n')
+            print(self.predictor.leaderboard()[['model', 'score_val', 'eval_metric']])
 
-        print(f"Event rates: {self.y_train.sum()/len(self.y_train):.3f} (train) {self.y_test.sum()/len(self.y_test):.3f} (test)")
-        print(f"Train/test: {len(self.y_train)}/{len(self.y_test)}")
+            if self.predictor.problem_type == 'binary':
+                plot_classification_diagnostics(self.predictor, self.X_test, self.y_test, self.data_columns)
+            elif self.predictor.problem_type == 'regression':
+                plot_regression_diagnostics(self.predictor, self.X_test, self.y_test, self.data_columns)
+   
 
-        # initialize saving best_models, best_params
-        self.best_models, self.best_params, self.scores, self.best_metric, self.cv_results = {}, {}, {}, {}, {}
-        
-        kwargs = {'scoring': self.metrics, 
-                  'n_jobs': -1, 
-                  'cv': self.cv, 
-                  'refit': self.metrics[-1], 
-                  'error_score': 0.,
-                  'return_train_score': True}
-        
-        if 'random' in self.search:
-            kwargs['n_iter'] = n_iter
+            self._plot_feature_importance()
 
-        with ThreadPoolExecutor(max_workers=None) as executor:
-            future_to_key = {executor.submit(self._evaluate_model, *[k, kwargs]): k for k in self.models}
-            for future in as_completed(future_to_key):
-                k = future_to_key[future]
-                try:
-                    result = future.result()
-                    self.cv_results[result['model_key']] = result['cv_results']
-                    self.best_models[result['model_key']] = result['best_estimator']
-                    self.scores[result['model_key']] = result['scores']
-
-                    self._display_results(result)
-
-                except Exception as exc:
-                    print(f"Model {k} generated an exception: {exc}")
-        
-        best_idx = np.argmax(list(self.best_metric.values()))            
-        model_k = list(self.best_metric.keys())[best_idx]
-        self.best_model = self.best_models[model_k]
-        print(f"\n\nBest model: {self.best_model} with {self.metrics[len(self.metrics)-1]} {self.best_metric[model_k]:.4f}")
-        print(f"Parameters: {self.best_model.get_params()}")
-
-        return
-
-if __name__ == '__main__':
-
-    # Example usage
-
-    automl = AutoMLSupervised(task="classifier")
-
-    df = pd.read_csv('data/MAR_per_OAR_for_patients.csv')
-
-    X = df[['95HD', 'VolDice', 'SurfDice','JaccardIndex', 'APL', 'FNPL', 'FNV']]
-    y = df['MAR'] > 3.5
-
-    automl.fit(X, y)
-
+    
