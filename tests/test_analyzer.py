16f5d3180d6179e315c7a8cd82067cea49679581
Sejin Kim <hello@sejin.kim>
2024-11-13 15:15:51 -0500

    working tests

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -1,5 +1,6 @@
 from AutoML.analyzer import Analyzer
 import pytest
+import numpy as np
 import pandas as pd
 import os
 import shutil
@@ -9,10 +10,11 @@ def sample_data():
     data = {
         'A': [1, 2, 3, 4, 5],
         'B': [5, 4, 3, 2, 1],
-        'C': ['a', 'b', 'c', 'a', 'c'],
+        'C': ['a', 'b', 'a', 'a', 'b'],
         'D': [None, 2, None, 4, 5]
     }
-    return pd.DataFrame(data)
+    df= pd.concat([pd.DataFrame(data), pd.DataFrame(data), pd.DataFrame(data)], axis=0)
+    return df.reset_index()
 
 @pytest.fixture
 def tmpdir():
@@ -57,7 +59,7 @@ def test_replace_missing(analyzer):
     analyzer.categorical_columns = []
     analyzer._replace_missing()
     assert analyzer.data['D'].isna().sum() == 0
-    assert analyzer.data['D'].iloc[0] == analyzer.data['D'].mean()
+    assert np.isclose(analyzer.data['D'].iloc[0], analyzer.data['D'].mean(), rtol=1e-4)
 
 def test_run_janitor(analyzer):
     # _infer_types is always run inside _run_janitor
@@ -69,7 +71,7 @@ def test_run_janitor(analyzer):
 def test_create_multiplots(analyzer):
     analyzer.categorical_columns = ['C']
     analyzer.continuous_columns = ['A', 'B']
-    analyzer.umap_data = pd.DataFrame({'UMAP1': [1, 2, 3, 4, 5], 'UMAP2': [5, 4, 3, 2, 1]})
+    analyzer.umap_data = pd.DataFrame.from_dict({'UMAP1': [i for i in range(1, 16)], 'UMAP2': [j for j in range(15, 0, -1)]}).to_numpy()
     analyzer._create_multiplots()
     assert len(analyzer.multiplots) > 0
 
