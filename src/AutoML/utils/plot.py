0e968548dfa2d13ca42b9b216b10bca2b3fa7cf0
Joshua Siraj <joshua.siraj@ryerson.ca>
2024-12-03 12:48:25 -0500

    feat: added train to explainer plots

--- src/AutoML/utils/plot.py
+++ src/AutoML/utils/plot.py
@@ -248,29 +248,39 @@ def bootstrap_metric(y_test, y_pred, f, nsamples=100):
         values.append(val)
     return values 
 
-def plot_violin_of_bootsrapped_metrics(predictor, X_test, y_test, X_val, y_val, output_dir):
+def plot_violin_of_bootsrapped_metrics(predictor, X_test, y_test, X_val, y_val, X_train, y_train, output_dir):
         
     # Define metrics based on the problem type
     if predictor.problem_type == 'regression':
         metrics = [('R Squared', r2_score), ('Root Mean Squared Error', root_mean_squared_error)]
     else:
-        metrics = [('ROC-AUC', roc_auc_score), ('Precision', average_precision_score)]
+        metrics = [('ROC-AUC', roc_auc_score), ('PR-AUC', average_precision_score)]
 
     # Prepare lists for DataFrame
     results = []
 
     # Loop through models and metrics to compute bootstrapped values
     for model_name in predictor.model_names():
-        for metric_name, metric_func in metrics:
-            # Compute bootstrapped metrics for the test set
+        
+        if predictor.problem_type == 'regression':
             y_pred_test = predictor.predict(X_test, model=model_name)
-            test_values = bootstrap_metric(y_test.to_numpy(), y_pred_test.to_numpy(), metric_func)
-            results.extend([(model_name, metric_name, 'Test', val) for val in test_values])
-
-            # Compute bootstrapped metrics for the validation set
             y_pred_val = predictor.predict(X_val, model=model_name)
+            y_pred_train = predictor.predict(X_train, model=model_name)
+        else:
+            y_pred_test = predictor.predict_proba(X_test, model=model_name).iloc[:, 1]
+            y_pred_val = predictor.predict_proba(X_val, model=model_name).iloc[:, 1]
+            y_pred_train = predictor.predict_proba(X_train, model=model_name).iloc[:, 1]
+
+        for metric_name, metric_func in metrics:
+            
+            test_values = bootstrap_metric(y_test.to_numpy(), y_pred_test.to_numpy(), metric_func)
+            results.extend([(model_name, metric_name, 'Test', value) for value in test_values])
+            
             val_values = bootstrap_metric(y_val.to_numpy(), y_pred_val.to_numpy(), metric_func)
-            results.extend([(model_name, metric_name, 'Validation', val) for val in val_values])
+            results.extend([(model_name, metric_name, 'Validation', value) for value in val_values])
+            
+            train_values = bootstrap_metric(y_train.to_numpy(), y_pred_train.to_numpy(), metric_func)
+            results.extend([(model_name, metric_name, 'Train', value) for value in train_values])
 
     # Create a results DataFrame
     result_df = pd.DataFrame(results, columns=['model', 'metric', 'data_split', 'value'])
@@ -290,24 +300,24 @@ def plot_violin_of_bootsrapped_metrics(predictor, X_test, y_test, X_val, y_val, 
     sns.set_theme(style="whitegrid")
     g = sns.FacetGrid(
         result_df,
-        row="metric",
-        col="data_split",
+        col="metric",
+        row="data_split",
         margin_titles=True,
         height=4,
         aspect=1.5,
-        sharex=False
+        sharex=True
     )
 
     # Create violin plots with sorted models
     def violin_plot(data, **kwargs):
         metric, data_split = data.iloc[0][['metric', 'data_split']]
         order = model_order.get((metric, data_split), None)
-        sns.violinplot(data=data, x="value", y="model", order=order, linewidth=1, **kwargs)
+        sns.violinplot(data=data, x="value", y="model", linewidth=1, order=order, **kwargs)
 
     g.map_dataframe(violin_plot)
 
     # Adjust the titles and axis labels
-    g.set_titles(row_template="{row_name}", col_template="{col_name} Data")
+    g.set_titles(row_template="{row_name} Data", col_template="{col_name}")
     g.set_axis_labels("Metric Value", "Model")
 
     # Add overall title and adjust layout
@@ -366,13 +376,13 @@ def plot_regression_diagnostics(y_true, y_pred, output_dir):
     plot_residuals(y_true, y_pred)
     plot_residual_histogram(residuals)
 
-def plot_classification_diagnostics(y_true, y_pred, y_val, y_val_pred, output_dir):
+def plot_classification_diagnostics(y_true, y_pred, y_val, y_val_pred, y_train, y_train_pred, output_dir):
     """
     Generates diagnostic plots for a classification model.
 
     """
 
-    plot_epic_copy(y_true.to_numpy(), y_pred.to_numpy(), y_val.to_numpy(), y_val_pred.to_numpy(), output_dir)
+    plot_epic_copy(y_true.to_numpy(), y_pred.to_numpy(), y_val.to_numpy(), y_val_pred.to_numpy(), y_train.to_numpy(), y_train_pred.to_numpy() , output_dir)
 
     conf_matrix = confusion_matrix(y_true, y_pred.apply(lambda x: 1 if x >= 0.5 else 0))
 
@@ -401,7 +411,7 @@ def _bin_class_curve(y_true, y_pred):
 
     return fps, tps, y_pred[threshold_idxs]
 
-def plot_epic_copy(y_test, y_pred, y_val, y_val_pred, output_dir):
+def plot_epic_copy(y_test, y_pred, y_val, y_val_pred, y_train, y_train_pred, output_dir):
     # Compute test metrics
     fpr_test, tpr_test, thresholds_roc_test = roc_curve(y_test, y_pred)
     roc_auc_test = roc_auc_score(y_test, y_pred)
@@ -416,42 +426,52 @@ def plot_epic_copy(y_test, y_pred, y_val, y_val_pred, output_dir):
     average_precision_val = average_precision_score(y_val, y_val_pred)
     prob_true_val, prob_pred_val = calibration_curve(y_val, y_val_pred, n_bins=10, strategy='uniform')
     
+    # Compute train metrics
+    fpr_train, tpr_train, thresholds_roc_train = roc_curve(y_train, y_train_pred)
+    roc_auc_train = roc_auc_score(y_train, y_train_pred)
+    precision_train, recall_train, thresholds_pr_train = precision_recall_curve(y_train, y_train_pred)
+    average_precision_train = average_precision_score(y_train, y_train_pred)
+    prob_true_train, prob_pred_train = calibration_curve(y_train, y_train_pred, n_bins=10, strategy='uniform')
+
+    # Compute confidence intervals
     roc_conf_test = [round(val, 2) for val in np.percentile(bootstrap_metric(y_test, y_pred, roc_auc_score), (2.5, 97.5))]
     roc_conf_val = [round(val, 2) for val in np.percentile(bootstrap_metric(y_val, y_val_pred, roc_auc_score), (2.5, 97.5))]
+    roc_conf_train = [round(val, 2) for val in np.percentile(bootstrap_metric(y_train, y_train_pred, roc_auc_score), (2.5, 97.5))]
+
     precision_conf_test = [round(val, 2) for val in np.percentile(bootstrap_metric(y_test, y_pred, average_precision_score), (2.5, 97.5))]
     precision_conf_val = [round(val, 2) for val in np.percentile(bootstrap_metric(y_val, y_val_pred, average_precision_score), (2.5, 97.5))]
-    
+    precision_conf_train = [round(val, 2) for val in np.percentile(bootstrap_metric(y_train, y_train_pred, average_precision_score), (2.5, 97.5))]
+
     # Set Seaborn style
     sns.set_theme(style="whitegrid")
 
-    plt.figure(figsize=(20, 10))
+    plt.figure(figsize=(37.5, 10))
 
     # 1. ROC Curve
-    plt.subplot(2, 4, 1)
+    plt.subplot(2, 5, 1)
     sns.lineplot(x=fpr_test, y=tpr_test, label=f"Test AUROC = {roc_auc_test:.2f} {roc_conf_test}", color="blue")
     sns.lineplot(x=fpr_val, y=tpr_val, label=f"Validation AUROC = {roc_auc_val:.2f} {roc_conf_val}", color="orange")
-    plt.fill_between(fpr_test, tpr_test - (roc_conf_test[1] - roc_auc_test), tpr_test + (roc_conf_test[1] - roc_auc_test), color='blue', alpha=0.2)
-    plt.fill_between(fpr_val, tpr_val - (roc_conf_val[1] - roc_auc_val), tpr_val + (roc_conf_val[1] - roc_auc_val), alpha=0.2, color='orange')
+    sns.lineplot(x=fpr_train, y=tpr_train, label=f"Train AUROC = {roc_auc_train:.2f} {roc_conf_train}", color="green")
     plt.xlabel("1 - Specificity")
     plt.ylabel("Sensitivity")
     plt.title("ROC Curve")
     plt.legend()
 
     # 2. Precision-Recall Curve
-    plt.subplot(2, 4, 2)
+    plt.subplot(2, 5, 2)
     sns.lineplot(x=recall_test, y=precision_test, label=f"Test AUC-PR = {average_precision_test:.2f} {precision_conf_test}", color="blue")
-    sns.lineplot(x=recall_val, y=precision_val, label=f"Validation AUC-PR = {average_precision_val:.2f} {precision_conf_test}", color="orange")
-    plt.fill_between(recall_test, precision_test - (precision_conf_test[1] - average_precision_test), precision_test + (precision_conf_test[1] - average_precision_test), color='blue', alpha=0.2)
-    plt.fill_between(recall_val, precision_val - (precision_conf_val[1] - average_precision_val), precision_val + (precision_conf_val[1] - average_precision_val), color='orange', alpha=0.2)
+    sns.lineplot(x=recall_val, y=precision_val, label=f"Validation AUC-PR = {average_precision_val:.2f} {precision_conf_val}", color="orange")
+    sns.lineplot(x=recall_train, y=precision_train, label=f"Train AUC-PR = {average_precision_train:.2f} {precision_conf_train}", color="green")
     plt.xlabel("Recall")
     plt.ylabel("Precision")
     plt.title("Precision-Recall Curve")
     plt.legend()
 
     # 3. Calibration Curve
-    plt.subplot(2, 4, 3)
+    plt.subplot(2, 5, 6)
     sns.lineplot(x=prob_pred_test, y=prob_true_test, label="Test Calibration Curve", color="blue", marker='o')
     sns.lineplot(x=prob_pred_val, y=prob_true_val, label="Validation Calibration Curve", color="orange", marker='o')
+    sns.lineplot(x=prob_pred_train, y=prob_true_train, label="Train Calibration Curve", color="green", marker='o')
     sns.lineplot(x=[0, 1], y=[0, 1], linestyle="--", label="Perfect Calibration", color="gray")
     plt.xlabel("Predicted Probability")
     plt.ylabel("Observed Probability")
@@ -467,37 +487,63 @@ def plot_epic_copy(y_test, y_pred, y_val, y_val_pred, output_dir):
     sens_val = tps / sum(y_val)
     flag_rate_val = (tps + fps) / len(y_val)
 
-    plt.subplot(2, 4, 4)
+    fps, tps, _ = _bin_class_curve(y_train, y_train_pred)
+    sens_train = tps / sum(y_train)
+    flag_rate_train = (tps + fps) / len(y_train)
+
+    plt.subplot(2, 5, 7)
     sns.lineplot(x=flag_rate_test, y=sens_test, label="Test", color="blue")
     sns.lineplot(x=flag_rate_val, y=sens_val, label="Validation", color="orange")
+    sns.lineplot(x=flag_rate_train, y=sens_train, label="Train", color="green")
     plt.xlabel('Flag Rate')
     plt.ylabel('Sensitivity')
     plt.title('Sensitivity/Flag Curve')
     plt.legend()
 
     # 5. Sensitivity, Specificity, PPV by Threshold
+    # Test Metrics
     sensitivity_test = tpr_test
     specificity_test = 1 - fpr_test
     ppv_test = precision_test[:-1]
+
+    # Validation Metrics
     sensitivity_val = tpr_val
     specificity_val = 1 - fpr_val
     ppv_val = precision_val[:-1]
-    plt.subplot(2, 4, 5)
+
+    # Train Metrics
+    sensitivity_train = tpr_train
+    specificity_train = 1 - fpr_train
+    ppv_train = precision_train[:-1]
+
+    # Plot Test Metrics
+    plt.subplot(2, 5, 3)
     sns.lineplot(x=thresholds_roc_test, y=sensitivity_test, label="Test Sensitivity", color="blue")
     sns.lineplot(x=thresholds_roc_test, y=specificity_test, label="Test Specificity", color="green")
     sns.lineplot(x=thresholds_pr_test, y=ppv_test, label="Test PPV", color="magenta")
     plt.xlabel("Threshold")
     plt.ylabel("Metric")
-    plt.title("Metrics by Threshold")
+    plt.title("Test Metrics by Threshold")
     plt.legend()
 
-    plt.subplot(2, 4, 6)
+    # Plot Validation Metrics
+    plt.subplot(2, 5, 4)
     sns.lineplot(x=thresholds_roc_val, y=sensitivity_val, label="Validation Sensitivity", linestyle="--", color="orange")
     sns.lineplot(x=thresholds_roc_val, y=specificity_val, label="Validation Specificity", linestyle="--", color="darkgreen")
     sns.lineplot(x=thresholds_pr_val, y=ppv_val, label="Validation PPV", linestyle="--", color="pink")
     plt.xlabel("Threshold")
     plt.ylabel("Metric")
-    plt.title("Metrics by Threshold")
+    plt.title("Validation Metrics by Threshold")
+    plt.legend()
+
+    # Plot Train Metrics
+    plt.subplot(2, 5, 5)
+    sns.lineplot(x=thresholds_roc_train, y=sensitivity_train, label="Train Sensitivity", linestyle=":", color="purple")
+    sns.lineplot(x=thresholds_roc_train, y=specificity_train, label="Train Specificity", linestyle=":", color="brown")
+    sns.lineplot(x=thresholds_pr_train, y=ppv_train, label="Train PPV", linestyle=":", color="cyan")
+    plt.xlabel("Threshold")
+    plt.ylabel("Metric")
+    plt.title("Train Metrics by Threshold")
     plt.legend()
 
     # 6. Histogram of Predicted Probabilities
@@ -510,11 +556,13 @@ def plot_epic_copy(y_test, y_pred, y_val, y_val_pred, output_dir):
         _get_highest_bin_count(y_pred[y_test == 0], bins=20),
         _get_highest_bin_count(y_pred[y_test == 1], bins=20),
         _get_highest_bin_count(y_val_pred[y_val == 0], bins=20),
-        _get_highest_bin_count(y_val_pred[y_val == 1], bins=20)
+        _get_highest_bin_count(y_val_pred[y_val == 1], bins=20),
+        _get_highest_bin_count(y_train_pred[y_train == 0], bins=20),
+        _get_highest_bin_count(y_train_pred[y_train == 1], bins=20)
     )
     highest_bin_count += highest_bin_count//20
     
-    plt.subplot(2, 4, 7)
+    plt.subplot(2, 5, 8)
     sns.histplot(y_pred[y_test == 0], bins=20, alpha=0.7, label="Test Actual False", color='blue', kde=False)
     sns.histplot(y_pred[y_test == 1], bins=20, alpha=0.7, label="Test Actual True", color='magenta', kde=False)
     plt.xlabel("Predicted Probability")
@@ -523,9 +571,18 @@ def plot_epic_copy(y_test, y_pred, y_val, y_val_pred, output_dir):
     plt.ylim(0, highest_bin_count)
     plt.legend()
 
-    plt.subplot(2, 4, 8)
+    plt.subplot(2, 5, 9)
     sns.histplot(y_val_pred[y_val == 0], bins=20, alpha=0.5, label="Validation Actual False", color='orange', kde=False)
     sns.histplot(y_val_pred[y_val == 1], bins=20, alpha=0.5, label="Validation Actual True", color='pink', kde=False)
+    plt.xlabel("Predicted Probability")
+    plt.ylabel("Count")
+    plt.title("Histogram of Predicted Probabilities")
+    plt.ylim(0, highest_bin_count)
+    plt.legend()
+
+    plt.subplot(2, 5, 10)
+    sns.histplot(y_train_pred[y_train == 0], bins=20, alpha=0.5, label="Train Actual False", color='green', kde=False)
+    sns.histplot(y_train_pred[y_train == 1], bins=20, alpha=0.5, label="Train Actual True", color='purple', kde=False)
     plt.xlabel("Predicted Probability")
     plt.ylabel("Count")
     plt.title("Histogram of Predicted Probabilities")
