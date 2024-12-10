3ecf85476b862e2679373d27b883dd2ed8104696
Joshua Siraj <joshua.siraj@ryerson.ca>
2024-12-10 15:20:29 -0500

    tests: updated to use pathlib instead of os

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -1,8 +1,8 @@
 from AutoML.analyzer import Analyzer
+from pathlib import Path
 import pytest
 import numpy as np
 import pandas as pd
-import os
 import shutil
 
 @pytest.fixture
@@ -18,28 +18,24 @@ def sample_data():
 
 @pytest.fixture
 def tmpdir():
-    temp_path = "./tests/tmp"
-    if not os.path.exists(temp_path):
-        os.makedirs(temp_path, exist_ok=True)
-    else:
-        for file in os.listdir(temp_path):
-            file_path = os.path.join(temp_path, file)
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-            
+    temp_path = Path("./tests/tmp")
+    temp_path.mkdir(parents=True, exist_ok=True)
+
+    for file in temp_path.iterdir():
+        file_path = temp_path / file
+        if file_path.is_file() or file_path.is_symlink():
+            file_path.unlink() 
+        elif file_path.is_dir():
+            shutil.rmtree(file_path) 
+                    
     yield temp_path
-    
-    # Cleanup after tests
-    # shutil.rmtree(temp_path, ignore_errors=True)
 
 @pytest.fixture
 def analyzer(sample_data, tmpdir):
-    config_file = os.path.join(tmpdir, 'config.yaml')
+    config_file = tmpdir / 'config.yaml'
     output_dir = tmpdir
-    if "data.csv" not in os.listdir(output_dir):
-        sample_data.to_csv(os.path.join(output_dir, 'data.csv'), index=False)
+    if "data.csv" not in output_dir.iterdir():
+        sample_data.to_csv(output_dir / 'data.csv', index=False)
     return Analyzer(data=sample_data, output_dir=output_dir)
 
 def test_analyzer_initialization(analyzer, sample_data):
@@ -72,8 +68,8 @@ def test_create_multiplots(analyzer):
 
 def test_run(analyzer):
     analyzer.run()
-    assert os.path.exists(os.path.join(analyzer.output_dir, 'tableone.csv'))
-    assert os.path.exists(os.path.join(analyzer.output_dir, 'updated_data.csv'))
-    assert os.path.exists(os.path.join(analyzer.output_dir, 'pearson_correlation.png'))
-    assert os.path.exists(os.path.join(analyzer.output_dir, 'spearman_correlation.png'))
-    assert os.path.exists(os.path.join(analyzer.output_dir, 'multiplots'))
+    assert (analyzer.output_dir / 'tableone.csv').exists()
+    assert (analyzer.output_dir / 'updated_data.csv').exists()
+    assert (analyzer.output_dir / 'pearson_correlation.png').exists()
+    assert (analyzer.output_dir / 'spearman_correlation.png').exists()
+    assert (analyzer.output_dir / 'multiplots').exists()
