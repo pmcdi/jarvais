27c37c6be0cb3cb61502d365fe910a03217cc26d
Joshua Siraj <joshua.siraj@ryerson.ca>
2025-01-08 11:14:17 -0500

    feat: run bias analysis on all sens feats

--- src/AutoML/utils/bias.py
+++ src/AutoML/utils/bias.py
@@ -1,60 +1,49 @@
 import inspect, re
-from functools import partial
+from typing import List
 
+from functools import partial
 import pandas as pd
 from tabulate import tabulate
 import fairlearn.metrics as fm
 
-
-def infer_sensitive_features(data: pd.DataFrame) -> list:
+def infer_sensitive_features(data: pd.DataFrame) -> dict:
     """
     Infers potentially sensitive features from a DataFrame.
-
-    Parameters
-    ----------
-    data : pd.DataFrame
-        The input dataset.
-        
-    Returns
-    -------
-    sensitive_features : list
-        List of column names identified as potentially sensitive.
     """
-    # Common sensitive keywords
     sensitive_keywords = [
         "gender", "sex", "age", "race", "ethnicity", "income",
         "religion", "disability", "nationality", "language",
-        "marital", "citizen", "veteran", "status", "orientation"
+        "marital", "citizen", "veteran", "status", "orientation",
         "disease", "regimen", "disease_site"
     ]
 
-    sensitive_features = []
+    sensitive_features = {
+        col for col in data.columns 
+        if any(re.search(rf"\b{keyword}\b", col, re.IGNORECASE) for keyword in sensitive_keywords)
+    }
 
-    # Check column names for sensitive keywords
-    for col in data.columns:
-        if any(re.search(rf"\b{keyword}\b", col, re.IGNORECASE) for keyword in sensitive_keywords):
-            sensitive_features.append(col)
-
-    # Remove duplicates
-    sensitive_features = list(set(sensitive_features))
-
-    return sensitive_features
+    return {sens_feat: data[sens_feat] for sens_feat in sensitive_features}
 
 def get_metric(metric, sensitive_features=None):
     fn = getattr(fm, metric)
     params = inspect.signature(fn).parameters
-    if 'sensitive_features' in params and sensitive_features is not None:
-        return partial(fn, sensitive_features=sensitive_features)
-    else:
-        return fn
+    return partial(fn, sensitive_features=sensitive_features) if 'sensitive_features' in params and sensitive_features else fn
 
 class BiasExplainer():
-    def __init__(self,
-                 y_true, y_pred, sensitive_features: dict, metrics=['mean_prediction', 'false_positive_rate'], **kwargs):
-        
+    def __init__(
+            self, 
+            y_true: pd.DataFrame, 
+            y_pred: pd.DataFrame, 
+            sensitive_features: dict, 
+            metrics: list = ['mean_prediction', 'false_positive_rate'], 
+            **kwargs: dict
+        ) -> None:
+
         self.y_true = y_true
         self.y_pred = y_pred
-
+        self.metrics = metrics
+        self.kwargs = kwargs
+        
         # Convert sensitive_features to DataFrame or leave as Series
         if isinstance(sensitive_features, pd.DataFrame) or isinstance(sensitive_features, pd.Series):
             self.sensitive_features = sensitive_features
@@ -67,28 +56,34 @@ class BiasExplainer():
                 self.sensitive_features = pd.DataFrame(sensitive_features, columns=['sensitive_feature'])
         else:
             raise ValueError("sensitive_features must be a pandas DataFrame, Series, dictionary or list")
-        
-        self.largest_features   = self.sensitive_features.groupby(self.sensitive_features.columns.tolist()).size().idxmax()
-        self.metrics            = {metric: get_metric(metric, sensitive_features=sensitive_features) for metric in metrics}
-        self.metric_frame       = self.get_metric_frame(**kwargs)
 
-    def get_metric_frame(self, **kwargs):
-        return fm.MetricFrame(metrics=self.metrics,
-                              y_true=self.y_true,
-                              y_pred=self.y_pred,
-                              sensitive_features=self.sensitive_features,
-                              **kwargs)
+    def run(
+            self, 
+            relative: bool = False, 
+            fairness_threshold: float = 1.2
+        ) -> List[pd.DataFrame]:
 
-    def run(self, relative: bool=False):
-        self.results = pd.DataFrame(self.metric_frame.by_group.T, index=self.metrics.keys())
-        if relative:
-            self.run_relative_largest()
+        self.results = []
+        for sensitive_feature in self.sensitive_features.columns:
+            largest_feature = self.sensitive_features[sensitive_feature].mode().iloc[0]
+            _metrics = {metric: get_metric(metric, sensitive_features=self.sensitive_features[sensitive_feature]) for metric in self.metrics}
 
-        print("Here are the bias subgroup analysis results:\n")
-        print(tabulate(self.results, headers='keys', tablefmt='fancy_grid'))
+            metric_frame = fm.MetricFrame(
+                metrics=_metrics, 
+                y_true=self.y_true, 
+                y_pred=self.y_pred, 
+                sensitive_features=self.sensitive_features[sensitive_feature], 
+                **self.kwargs
+            )
+            result = pd.DataFrame(metric_frame.by_group.T, index=_metrics.keys())
+            
+            if relative:
+                results_relative = result.T / result[largest_feature]
+                results_relative = results_relative.applymap(lambda x: f"{x} ✅" if x <= fairness_threshold or 1/x <= fairness_threshold else f"{x} ❌")
+                result = pd.concat([result, results_relative.T.rename(index=lambda x: f"Relative {x}")])
 
-    def run_relative_largest(self):
-        print(f"Calculating relative metric to the largest subgroup: {self.largest_features}")
-        results_relative = self.results.T/self.results[self.largest_features]
-        results_relative = results_relative.applymap(lambda x: f"{x} ✅" if x > 0.9 else f"{x} ❌")
-        self.results = pd.concat([self.results, results_relative.T.rename(index=lambda x: f"Relative {x}")])
+            self.results.append(result)
+            print(f"Subgroup Analysis({sensitive_feature.title()})")
+            print(f'{tabulate(result.iloc[:, :4], headers='keys', tablefmt='fancy_grid')}\n')
+
+        return self.results
