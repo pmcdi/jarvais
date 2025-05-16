72e13846c05a7d773f48ec568c7824e12d0cc98b
Joshua Siraj <72413722+JoshuaSiraj@users.noreply.github.com>
2025-05-16 11:34:31 -0400

    Revert "feat!: analyzer refactor"

--- src/jarvais/analyzer/modules/__init__.py
+++
@@ -1,7 +0,0 @@
-from .missingness import MissingnessModule
-from .outlier import OutlierModule
-from .visualization import VisualizationModule
-from .encoding import OneHotEncodingModule
-
-__all__ = ["MissingnessModule", "OutlierModule", "VisualizationModule", "OneHotEncodingModule"]
-
--- tests/analyzer/modules/test_outlier.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import pytest
-
-from jarvais.analyzer.modules.outlier import OutlierModule
-
-
-def test_outlier_module_executes():
-    pass # TODO
